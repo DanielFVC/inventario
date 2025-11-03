7feb8368186e00ed443c46038c2bf6d256aa7033
DanielFVC <danielfvillalba@gmail.com>
2025-11-03 17:29:14 -0500

    Enrutador y controladores

--- src/app.js
+++ src/app.js
@@ -1,11 +1,15 @@
 /*IMPORTAMOS AL FRAMEWORK EXPRESS*/
 import express from "express";
+import categoriasRoutes from "./routes/categorias.routes.js";
 
 /* asignamos a app toda funcionalidad para mi server web*/
 const app = express();
 
 /*setear un puerto a mi web server*/
 app.set("port",5000);
+
+/*routes*/
+app.use("/api/categorias",categoriasRoutes);
 
 /*hacemos disponible a mi server app para toda la aplicación*/
 export default app;
